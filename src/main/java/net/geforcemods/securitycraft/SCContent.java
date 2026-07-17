7f2cda1526f6d2fb30835506a5c6d68281beed6e
VerkoSK <business.verko@gmail.com>
2026-07-18 00:49:28 +0200

    Add reinforced block set (226 blocks) from upstream
    172 full-cube + 51 pillar + reinforced/tinted glass + iron bars + bamboo fence.
    Assets copied from upstream SecurityCraft; registration via data table in SCContent.
    Creative tab now full. Complex shapes + functional blocks roadmapped. Builds green.

--- src/main/java/net/geforcemods/securitycraft/SCContent.java
+++ src/main/java/net/geforcemods/securitycraft/SCContent.java
@@ -20,56 +20,288 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.FenceBlock;
+import net.minecraft.world.level.block.IronBarsBlock;
+import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 
-/** Central registration for all blocks, items, block entities and the creative tab. */
+/** Central registration. Registers the keypad plus the full reinforced-block set copied from upstream. */
 public class SCContent {
 	private static final List<ItemLike> TAB_ITEMS = new ArrayList<>();
+	public static final List<Block> GLASS_BLOCKS = new ArrayList<>();
 
-	// --- Blocks ---
-	public static final Block KEYPAD = new KeypadBlock(BlockBehaviour.Properties.of().strength(2.0F, 12000.0F).requiresCorrectToolForDrops());
-	public static final Block REINFORCED_STONE = reinforcedOpaque(SoundType.STONE);
-	public static final Block REINFORCED_COBBLESTONE = reinforcedOpaque(SoundType.STONE);
-	public static final Block REINFORCED_STONE_BRICKS = reinforcedOpaque(SoundType.STONE);
-	public static final Block REINFORCED_SMOOTH_STONE = reinforcedOpaque(SoundType.STONE);
-	public static final Block REINFORCED_OAK_PLANKS = reinforcedOpaque(SoundType.WOOD);
-	public static final Block REINFORCED_DIRT = reinforcedOpaque(SoundType.GRAVEL);
-	public static final Block REINFORCED_IRON_BLOCK = reinforcedOpaque(SoundType.METAL);
-	public static final Block REINFORCED_GLASS = reinforcedGlass();
-
-	// --- Block entities (assigned in init once the blocks are registered) ---
+	public static Block KEYPAD;
 	public static BlockEntityType<KeypadBlockEntity> KEYPAD_BLOCK_ENTITY;
 
-	// --- Creative tab ---
 	public static final ResourceKey<CreativeModeTab> TAB_KEY = ResourceKey.create(Registries.CREATIVE_MODE_TAB, id("general"));
+
+	// name, category (generated from upstream SecurityCraft reinforced blocks)
+	private static final String[][] REINFORCED = {
+			{"reinforced_acacia_log", "pillar"},
+			{"reinforced_acacia_planks", "cube"},
+			{"reinforced_acacia_wood", "pillar"},
+			{"reinforced_amethyst_block", "cube"},
+			{"reinforced_andesite", "cube"},
+			{"reinforced_bamboo_block", "pillar"},
+			{"reinforced_bamboo_fence", "fence"},
+			{"reinforced_bamboo_mosaic", "cube"},
+			{"reinforced_bamboo_planks", "cube"},
+			{"reinforced_basalt", "pillar"},
+			{"reinforced_birch_log", "pillar"},
+			{"reinforced_birch_planks", "cube"},
+			{"reinforced_birch_wood", "pillar"},
+			{"reinforced_black_concrete", "cube"},
+			{"reinforced_black_terracotta", "cube"},
+			{"reinforced_black_wool", "cube"},
+			{"reinforced_blackstone", "cube"},
+			{"reinforced_blue_concrete", "cube"},
+			{"reinforced_blue_ice", "cube"},
+			{"reinforced_blue_terracotta", "cube"},
+			{"reinforced_blue_wool", "cube"},
+			{"reinforced_bone_block", "pillar"},
+			{"reinforced_bricks", "cube"},
+			{"reinforced_brown_concrete", "cube"},
+			{"reinforced_brown_terracotta", "cube"},
+			{"reinforced_brown_wool", "cube"},
+			{"reinforced_calcite", "cube"},
+			{"reinforced_cherry_log", "pillar"},
+			{"reinforced_cherry_planks", "cube"},
+			{"reinforced_cherry_wood", "pillar"},
+			{"reinforced_chiseled_copper", "cube"},
+			{"reinforced_chiseled_deepslate", "cube"},
+			{"reinforced_chiseled_nether_bricks", "cube"},
+			{"reinforced_chiseled_polished_blackstone", "cube"},
+			{"reinforced_chiseled_quartz_block", "cube"},
+			{"reinforced_chiseled_red_sandstone", "cube"},
+			{"reinforced_chiseled_sandstone", "cube"},
+			{"reinforced_chiseled_stone_bricks", "cube"},
+			{"reinforced_chiseled_tuff", "cube"},
+			{"reinforced_chiseled_tuff_bricks", "cube"},
+			{"reinforced_clay", "cube"},
+			{"reinforced_coal_block", "cube"},
+			{"reinforced_coarse_dirt", "cube"},
+			{"reinforced_cobbled_deepslate", "cube"},
+			{"reinforced_cobblestone", "cube"},
+			{"reinforced_copper_block", "cube"},
+			{"reinforced_cracked_deepslate_bricks", "cube"},
+			{"reinforced_cracked_deepslate_tiles", "cube"},
+			{"reinforced_cracked_nether_bricks", "cube"},
+			{"reinforced_cracked_polished_blackstone_bricks", "cube"},
+			{"reinforced_cracked_stone_bricks", "cube"},
+			{"reinforced_crimson_hyphae", "pillar"},
+			{"reinforced_crimson_nylium", "cube"},
+			{"reinforced_crimson_planks", "cube"},
+			{"reinforced_crimson_stem", "pillar"},
+			{"reinforced_crying_obsidian", "cube"},
+			{"reinforced_cut_copper", "cube"},
+			{"reinforced_cut_red_sandstone", "cube"},
+			{"reinforced_cut_sandstone", "cube"},
+			{"reinforced_cyan_concrete", "cube"},
+			{"reinforced_cyan_terracotta", "cube"},
+			{"reinforced_cyan_wool", "cube"},
+			{"reinforced_dark_oak_log", "pillar"},
+			{"reinforced_dark_oak_planks", "cube"},
+			{"reinforced_dark_oak_wood", "pillar"},
+			{"reinforced_dark_prismarine", "cube"},
+			{"reinforced_deepslate", "pillar"},
+			{"reinforced_deepslate_bricks", "cube"},
+			{"reinforced_deepslate_tiles", "cube"},
+			{"reinforced_diamond_block", "cube"},
+			{"reinforced_diorite", "cube"},
+			{"reinforced_dirt", "cube"},
+			{"reinforced_dripstone_block", "cube"},
+			{"reinforced_emerald_block", "cube"},
+			{"reinforced_end_stone", "cube"},
+			{"reinforced_end_stone_bricks", "cube"},
+			{"reinforced_exposed_chiseled_copper", "cube"},
+			{"reinforced_exposed_copper", "cube"},
+			{"reinforced_exposed_cut_copper", "cube"},
+			{"reinforced_glowstone", "cube"},
+			{"reinforced_gold_block", "cube"},
+			{"reinforced_granite", "cube"},
+			{"reinforced_grass_path", "cube"},
+			{"reinforced_gravel", "cube"},
+			{"reinforced_gray_concrete", "cube"},
+			{"reinforced_gray_terracotta", "cube"},
+			{"reinforced_gray_wool", "cube"},
+			{"reinforced_green_concrete", "cube"},
+			{"reinforced_green_terracotta", "cube"},
+			{"reinforced_green_wool", "cube"},
+			{"reinforced_hardened_clay", "cube"},
+			{"reinforced_ice", "cube"},
+			{"reinforced_iron_bars", "pane"},
+			{"reinforced_iron_block", "cube"},
+			{"reinforced_jungle_log", "pillar"},
+			{"reinforced_jungle_planks", "cube"},
+			{"reinforced_jungle_wood", "pillar"},
+			{"reinforced_lapis_block", "cube"},
+			{"reinforced_light_blue_concrete", "cube"},
+			{"reinforced_light_blue_terracotta", "cube"},
+			{"reinforced_light_blue_wool", "cube"},
+			{"reinforced_light_gray_concrete", "cube"},
+			{"reinforced_light_gray_terracotta", "cube"},
+			{"reinforced_light_gray_wool", "cube"},
+			{"reinforced_lime_concrete", "cube"},
+			{"reinforced_lime_terracotta", "cube"},
+			{"reinforced_lime_wool", "cube"},
+			{"reinforced_magenta_concrete", "cube"},
+			{"reinforced_magenta_terracotta", "cube"},
+			{"reinforced_magenta_wool", "cube"},
+			{"reinforced_magma_block", "cube"},
+			{"reinforced_mangrove_log", "pillar"},
+			{"reinforced_mangrove_planks", "cube"},
+			{"reinforced_mangrove_wood", "pillar"},
+			{"reinforced_moss_block", "cube"},
+			{"reinforced_mossy_cobblestone", "cube"},
+			{"reinforced_mossy_stone_bricks", "cube"},
+			{"reinforced_mud", "cube"},
+			{"reinforced_mud_bricks", "cube"},
+			{"reinforced_nether_bricks", "cube"},
+			{"reinforced_nether_wart_block", "cube"},
+			{"reinforced_netherite_block", "cube"},
+			{"reinforced_netherrack", "cube"},
+			{"reinforced_oak_log", "pillar"},
+			{"reinforced_oak_planks", "cube"},
+			{"reinforced_oak_wood", "pillar"},
+			{"reinforced_obsidian", "cube"},
+			{"reinforced_ochre_froglight", "pillar"},
+			{"reinforced_orange_concrete", "cube"},
+			{"reinforced_orange_terracotta", "cube"},
+			{"reinforced_orange_wool", "cube"},
+			{"reinforced_oxidized_chiseled_copper", "cube"},
+			{"reinforced_oxidized_copper", "cube"},
+			{"reinforced_oxidized_cut_copper", "cube"},
+			{"reinforced_packed_ice", "cube"},
+			{"reinforced_packed_mud", "cube"},
+			{"reinforced_pearlescent_froglight", "pillar"},
+			{"reinforced_pink_concrete", "cube"},
+			{"reinforced_pink_terracotta", "cube"},
+			{"reinforced_pink_wool", "cube"},
+			{"reinforced_polished_andesite", "cube"},
+			{"reinforced_polished_basalt", "pillar"},
+			{"reinforced_polished_blackstone", "cube"},
+			{"reinforced_polished_blackstone_bricks", "cube"},
+			{"reinforced_polished_deepslate", "cube"},
+			{"reinforced_polished_diorite", "cube"},
+			{"reinforced_polished_granite", "cube"},
+			{"reinforced_polished_tuff", "cube"},
+			{"reinforced_prismarine", "cube"},
+			{"reinforced_prismarine_bricks", "cube"},
+			{"reinforced_purple_concrete", "cube"},
+			{"reinforced_purple_terracotta", "cube"},
+			{"reinforced_purple_wool", "cube"},
+			{"reinforced_purpur_block", "cube"},
+			{"reinforced_purpur_pillar", "pillar"},
+			{"reinforced_quartz_block", "cube"},
+			{"reinforced_quartz_bricks", "cube"},
+			{"reinforced_quartz_pillar", "pillar"},
+			{"reinforced_raw_copper_block", "cube"},
+			{"reinforced_raw_gold_block", "cube"},
+			{"reinforced_raw_iron_block", "cube"},
+			{"reinforced_red_concrete", "cube"},
+			{"reinforced_red_nether_bricks", "cube"},
+			{"reinforced_red_sand", "cube"},
+			{"reinforced_red_sandstone", "cube"},
+			{"reinforced_red_terracotta", "cube"},
+			{"reinforced_red_wool", "cube"},
+			{"reinforced_redstone_block", "cube"},
+			{"reinforced_rooted_dirt", "cube"},
+			{"reinforced_sand", "cube"},
+			{"reinforced_sandstone", "cube"},
+			{"reinforced_shroomlight", "cube"},
+			{"reinforced_smooth_basalt", "cube"},
+			{"reinforced_smooth_quartz", "cube"},
+			{"reinforced_smooth_red_sandstone", "cube"},
+			{"reinforced_smooth_sandstone", "cube"},
+			{"reinforced_smooth_stone", "cube"},
+			{"reinforced_snow_block", "cube"},
+			{"reinforced_soul_sand", "cube"},
+			{"reinforced_soul_soil", "cube"},
+			{"reinforced_spruce_log", "pillar"},
+			{"reinforced_spruce_planks", "cube"},
+			{"reinforced_spruce_wood", "pillar"},
+			{"reinforced_stone", "cube"},
+			{"reinforced_stone_bricks", "cube"},
+			{"reinforced_stone_polished_andesite", "cube"},
+			{"reinforced_stone_polished_granite", "cube"},
+			{"reinforced_stripped_acacia_log", "pillar"},
+			{"reinforced_stripped_acacia_wood", "pillar"},
+			{"reinforced_stripped_bamboo_block", "pillar"},
+			{"reinforced_stripped_birch_log", "pillar"},
+			{"reinforced_stripped_birch_wood", "pillar"},
+			{"reinforced_stripped_cherry_log", "pillar"},
+			{"reinforced_stripped_cherry_wood", "pillar"},
+			{"reinforced_stripped_crimson_hyphae", "pillar"},
+			{"reinforced_stripped_crimson_stem", "pillar"},
+			{"reinforced_stripped_dark_oak_log", "pillar"},
+			{"reinforced_stripped_dark_oak_wood", "pillar"},
+			{"reinforced_stripped_jungle_log", "pillar"},
+			{"reinforced_stripped_jungle_wood", "pillar"},
+			{"reinforced_stripped_mangrove_log", "pillar"},
+			{"reinforced_stripped_mangrove_wood", "pillar"},
+			{"reinforced_stripped_oak_log", "pillar"},
+			{"reinforced_stripped_oak_wood", "pillar"},
+			{"reinforced_stripped_spruce_log", "pillar"},
+			{"reinforced_stripped_spruce_wood", "pillar"},
+			{"reinforced_stripped_warped_hyphae", "pillar"},
+			{"reinforced_stripped_warped_stem", "pillar"},
+			{"reinforced_tinted_glass", "glass"},
+			{"reinforced_tuff", "cube"},
+			{"reinforced_tuff_bricks", "cube"},
+			{"reinforced_verdant_froglight", "pillar"},
+			{"reinforced_warped_hyphae", "pillar"},
+			{"reinforced_warped_nylium", "cube"},
+			{"reinforced_warped_planks", "cube"},
+			{"reinforced_warped_stem", "pillar"},
+			{"reinforced_warped_wart_block", "cube"},
+			{"reinforced_weathered_chiseled_copper", "cube"},
+			{"reinforced_weathered_copper", "cube"},
+			{"reinforced_weathered_cut_copper", "cube"},
+			{"reinforced_white_concrete", "cube"},
+			{"reinforced_white_terracotta", "cube"},
+			{"reinforced_white_wool", "cube"},
+			{"reinforced_yellow_concrete", "cube"},
+			{"reinforced_yellow_terracotta", "cube"},
+			{"reinforced_yellow_wool", "cube"}
+	};
 
 	public static ResourceLocation id(String name) {
 		return ResourceLocation.fromNamespaceAndPath(SecurityCraft.MODID, name);
 	}
 
 	public static void init() {
-		registerBlock("keypad", KEYPAD);
-		registerBlock("reinforced_stone", REINFORCED_STONE);
-		registerBlock("reinforced_cobblestone", REINFORCED_COBBLESTONE);
-		registerBlock("reinforced_stone_bricks", REINFORCED_STONE_BRICKS);
-		registerBlock("reinforced_smooth_stone", REINFORCED_SMOOTH_STONE);
-		registerBlock("reinforced_oak_planks", REINFORCED_OAK_PLANKS);
-		registerBlock("reinforced_dirt", REINFORCED_DIRT);
-		registerBlock("reinforced_iron_block", REINFORCED_IRON_BLOCK);
-		registerBlock("reinforced_glass", REINFORCED_GLASS);
+		KEYPAD = register("keypad", new KeypadBlock(BlockBehaviour.Properties.of().strength(2.0F, 12000.0F).requiresCorrectToolForDrops()));
+
+		for (String[] entry : REINFORCED)
+			registerReinforced(entry[0], entry[1]);
 
 		KEYPAD_BLOCK_ENTITY = Registry.register(BuiltInRegistries.BLOCK_ENTITY_TYPE, id("keypad"), FabricBlockEntityTypeBuilder.create(KeypadBlockEntity::new, KEYPAD).build());
 
 		registerCreativeTab();
 	}
 
-	private static void registerBlock(String name, Block block) {
+	private static void registerReinforced(String name, String category) {
+		Block block = switch (category) {
+			case "pillar" -> new RotatedPillarBlock(pillarProps(name));
+			case "glass" -> new BaseReinforcedBlock(glassProps());
+			case "pane" -> new IronBarsBlock(paneProps());
+			case "fence" -> new FenceBlock(fenceProps());
+			default -> new BaseReinforcedBlock(cubeProps(name));
+		};
+		register(name, block);
+
+		if (category.equals("glass"))
+			GLASS_BLOCKS.add(block);
+	}
+
+	private static Block register(String name, Block block) {
 		Registry.register(BuiltInRegistries.BLOCK, id(name), block);
 		BlockItem item = new BlockItem(block, new Item.Properties());
 		Registry.register(BuiltInRegistries.ITEM, id(name), item);
 		TAB_ITEMS.add(item);
+		return block;
 	}
 
 	private static void registerCreativeTab() {
@@ -81,18 +313,43 @@ public class SCContent {
 		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, TAB_KEY.location(), tab);
 	}
 
-	private static Block reinforcedOpaque(SoundType sound) {
-		return new BaseReinforcedBlock(BlockBehaviour.Properties.of().strength(2.0F, 12000.0F).requiresCorrectToolForDrops().sound(sound));
+	private static BlockBehaviour.Properties cubeProps(String name) {
+		boolean wood = name.contains("planks") || name.contains("mosaic") || name.contains("bookshelf");
+		boolean wool = name.contains("wool");
+		BlockBehaviour.Properties p = BlockBehaviour.Properties.of().strength(wood || wool ? 2.0F : 5.0F, 12000.0F).sound(wood ? SoundType.WOOD : wool ? SoundType.WOOL : SoundType.STONE);
+
+		if (!wood && !wool)
+			p.requiresCorrectToolForDrops();
+
+		return p;
 	}
 
-	private static Block reinforcedGlass() {
-		return new BaseReinforcedBlock(BlockBehaviour.Properties.of()
+	private static BlockBehaviour.Properties pillarProps(String name) {
+		boolean wood = name.contains("log") || name.contains("wood") || name.contains("stem") || name.contains("hyphae") || name.contains("bamboo");
+		BlockBehaviour.Properties p = BlockBehaviour.Properties.of().strength(wood ? 2.0F : 5.0F, 12000.0F).sound(wood ? SoundType.WOOD : SoundType.STONE);
+
+		if (!wood)
+			p.requiresCorrectToolForDrops();
+
+		return p;
+	}
+
+	private static BlockBehaviour.Properties glassProps() {
+		return BlockBehaviour.Properties.of()
 				.strength(1.5F, 12000.0F)
 				.sound(SoundType.GLASS)
 				.noOcclusion()
 				.isValidSpawn((state, level, pos, type) -> false)
 				.isRedstoneConductor((state, level, pos) -> false)
 				.isSuffocating((state, level, pos) -> false)
-				.isViewBlocking((state, level, pos) -> false));
+				.isViewBlocking((state, level, pos) -> false);
+	}
+
+	private static BlockBehaviour.Properties paneProps() {
+		return BlockBehaviour.Properties.of().strength(5.0F, 12000.0F).requiresCorrectToolForDrops().sound(SoundType.METAL).noOcclusion();
+	}
+
+	private static BlockBehaviour.Properties fenceProps() {
+		return BlockBehaviour.Properties.of().strength(2.0F, 12000.0F).sound(SoundType.WOOD);
 	}
 }
