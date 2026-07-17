2369f657e5a24fe9c9521445042a844b210476e4
VerkoSK <business.verko@gmail.com>
2026-07-18 01:36:31 +0200

    Add reinforced block set (226 blocks) on MC 1.21.7

--- src/main/java/net/geforcemods/securitycraft/SecurityCraftClient.java
+++ src/main/java/net/geforcemods/securitycraft/SecurityCraftClient.java
@@ -5,11 +5,6 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.geforcemods.securitycraft.network.OpenKeypadScreenPayload;
 import net.geforcemods.securitycraft.screen.KeypadScreen;
 
-/**
- * Client entrypoint. Wires the keypad screen packet. Reinforced-glass translucency is declared
- * natively in its block model ({@code "render_type": "minecraft:translucent"}) since Fabric's
- * BlockRenderLayerMap was removed once Mojang added model render types.
- */
 public class SecurityCraftClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
