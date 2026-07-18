3ace60380b43edba51d60123d12645581d444662
VerkoSK <business.verko@gmail.com>
2026-07-18 04:00:56 +0200

    Add reinforced grey tint + fence fix on MC 1.21.6

--- src/main/java/net/geforcemods/securitycraft/SecurityCraftClient.java
+++ src/main/java/net/geforcemods/securitycraft/SecurityCraftClient.java
@@ -2,12 +2,20 @@ package net.geforcemods.securitycraft;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.geforcemods.securitycraft.network.OpenKeypadScreenPayload;
 import net.geforcemods.securitycraft.screen.KeypadScreen;
+import net.minecraft.world.level.block.Block;
 
+/** Client entrypoint: keypad screen packet + reinforced grey tint. */
 public class SecurityCraftClient implements ClientModInitializer {
+	private static final int REINFORCED_TINT = 0x999999;
+
 	@Override
 	public void onInitializeClient() {
 		ClientPlayNetworking.registerGlobalReceiver(OpenKeypadScreenPayload.TYPE, (payload, context) -> context.client().execute(() -> context.client().setScreen(new KeypadScreen(payload.pos(), payload.setup(), payload.ownerName()))));
+
+		Block[] reinforced = SCContent.REINFORCED_BLOCKS.toArray(new Block[0]);
+		ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> tintIndex == 0 ? REINFORCED_TINT : -1, reinforced);
 	}
 }
