ac1905da385277d3e84e1e322ceca6e59aee6116
VerkoSK <business.verko@gmail.com>
2026-07-18 03:49:52 +0200

    Add reinforced grey tint + fence fix on MC 1.21.3

--- src/main/java/net/geforcemods/securitycraft/SecurityCraftClient.java
+++ src/main/java/net/geforcemods/securitycraft/SecurityCraftClient.java
@@ -1,17 +1,25 @@
 package net.geforcemods.securitycraft;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.geforcemods.securitycraft.network.OpenKeypadScreenPayload;
 import net.geforcemods.securitycraft.screen.KeypadScreen;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.client.renderer.RenderType;
 
+/** Client entrypoint: keypad screen packet + reinforced grey tint + glass render layer. */
 public class SecurityCraftClient implements ClientModInitializer {
+	private static final int REINFORCED_TINT = 0x999999;
+
 	@Override
 	public void onInitializeClient() {
 		ClientPlayNetworking.registerGlobalReceiver(OpenKeypadScreenPayload.TYPE, (payload, context) -> context.client().execute(() -> context.client().setScreen(new KeypadScreen(payload.pos(), payload.setup(), payload.ownerName()))));
+
+		Block[] reinforced = SCContent.REINFORCED_BLOCKS.toArray(new Block[0]);
+		ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> tintIndex == 0 ? REINFORCED_TINT : -1, reinforced);
+		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex == 0 ? REINFORCED_TINT : -1, reinforced);
 
 		for (Block glass : SCContent.GLASS_BLOCKS)
 			BlockRenderLayerMap.INSTANCE.putBlock(glass, RenderType.translucent());
